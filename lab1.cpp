560e92ed621ade75433ca633a254d1a7065337ca
MurikZ <143904061+MurikZ@users.noreply.github.com>
2024-04-22 01:39:47 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    deque <float> users{ 1,2.4,8 , 4.5,6 };
+    stack <float> stack{ users };
+
+    int elem1 = stack.top();
+    while (!stack.empty())
+    {
+        int a = stack.top();
+        stack.pop();
+        if (a > elem1)
+        {
+
+            elem1 = a;
+
+
+        }
+
+
+    }
+    cout << elem1;
+}
